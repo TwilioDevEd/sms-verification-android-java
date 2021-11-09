26a40fd3f12cb7de80c986cceebd10c3aafce1ec
Alejandro Vivanco <avivanco@twilio.com>
2021-11-09 11:27:19 -0500

    [DEVEDSB-98] Fix package error from error handler

--- src/main/java/com/twilio/androidsms/controllers/ErrorController.java
+++ src/main/java/com/twilio/androidsms/controllers/ErrorController.java
@@ -1,5 +1,3 @@
-package com.twilio.sampletemplatespring;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
